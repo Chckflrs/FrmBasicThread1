69c5a9013a24e98b3dd981af337a4a459ea238ae
flores.251829 <flores.251829@LAB-C-PC27>
2023-10-16 19:08:49 +0800

    Add project files.

---
+++ FrmBasicThread1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmBasicThread1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart delThread = new ThreadStart(MyTHreadClass.Thread1);
+            Thread threadA = new Thread(delThread);
+            threadA.Name = "Thread A";
+            Thread threadB = new Thread(delThread);
+            threadB.Name = "Thread B";
+            Console.WriteLine(label1.Text.ToString());
+            //label1.Text = "-Before the Thread Starts-";
+            threadA.Start();
+            threadB.Start();
+
+            threadA.Join();
+            threadB.Join();
+
+            label1.Text = "-End of Thread-";
+
+            Console.WriteLine(label1.Text.ToString());
+        }
+    }
+}
---
+++ FrmBasicThread1/MyTHreadClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrmBasicThread1
+{
+    internal class MyTHreadClass
+    {
+        public static void Thread1()
+        {
+            for (int Loopcount = 0; Loopcount <=5; Loopcount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: "+ thread.Name+ " = "+Loopcount );
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
